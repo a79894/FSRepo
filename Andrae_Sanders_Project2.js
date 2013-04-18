d7a6bd58cd235c50fceef1a07e25a873b0407fa1
andraesanders <andrerumal@fullsail.edu>
2013-04-18 13:00:58 -0500

    My Procedure

--- Andrae_Sanders_Project2.js
+++ Andrae_Sanders_Project2.js
@@ -4,26 +4,69 @@
 // 
 
 //My Variables
+
+
 //My strings
+
 var WakeUp = "I Wake up.";
+
 var WeatherCheck = "I check the weather on my iPhone.";
+
 var StayIn = "I'm staying home ";
+
 var True = "true";
+
 var BootCold = true;
+
 var FryeBoots = "\"Frye\" boots";
+
 var Sneakers = "I will wear my sneakers";
+
 var Freeze =  " surely, I will freeze to death!";
 
+var Sleeping = "I am sleeping";
+
+var WakingUp = "I am waking up!";
+
+ 
 //My numbers
 
 var temp = 30;
+
 var Time = 8 
 
 
 //My arrays
 
-var conditions = ["windy", "sunny", "rainy", "chilly", "breezy", "clear", "freezing"];
+var SnoozeTimes = ["10", "20", "30"]
+
+var conditions = ["windy", "sunny", "rainy", "chilly", "warm", "breezy", "clear", "freezing"];
 
 var ShirtColors = [ "blue", "yellow", "green", "red", "white", "black"];
 
-console.log (conditions, ShirtColors);
+
+//My Procedure
+	
+for (var temp = 0; temp <= 30; temp +=30) {
+	
+	console.log ("If it is " + temp + " degrees in my room, I'm staying in bed");
+};
+
+console.log ("If it is more than 30 degrees," + WakingUp );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
