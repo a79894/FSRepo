1c4092c2471f74070bf0ee23bfa0c00fc84b178a
andraesanders <andrerumal@fullsail.edu>
2013-04-18 10:51:15 -0500

    Variables and Arrays

--- Andrae_Sanders_Project2.js
+++ Andrae_Sanders_Project2.js
@@ -1,1 +1,29 @@
-alert("JavaScript works!");
+//Author: Andrae Sanders
+//SDI: 1304
+//Project 2
+// 
+
+//My Variables
+//My strings
+var WakeUp = "I Wake up.";
+var WeatherCheck = "I check the weather on my iPhone.";
+var StayIn = "I'm staying home ";
+var True = "true";
+var BootCold = true;
+var FryeBoots = "\"Frye\" boots";
+var Sneakers = "I will wear my sneakers";
+var Freeze =  " surely, I will freeze to death!";
+
+//My numbers
+
+var temp = 30;
+var Time = 8 
+
+
+//My arrays
+
+var conditions = ["windy", "sunny", "rainy", "chilly", "breezy", "clear", "freezing"];
+
+var ShirtColors = [ "blue", "yellow", "green", "red", "white", "black"];
+
+console.log (conditions, ShirtColors);
