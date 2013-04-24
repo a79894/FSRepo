f3119203f7fbdeab3f9eab9948d0df2824ef3d0d
andraesanders <andrerumal@fullsail.edu>
2013-04-24 16:26:00 -0500

    Define Batmobile

--- Andrae_Sanders_Project3.js
+++ Andrae_Sanders_Project3.js
@@ -3,21 +3,29 @@
 
 //Boolean nested conditional
 
-var BatmanSays = ["I need the BatSuit.", 
-			"To the BatCave!", 
-			"To the Batmobile!", 
-    		"I am ready to defeat the Joker." ,
-    		"I am not prepared to defeat the Joker"
+var BatmanSays = [
+	"I need the BatSuit.", 
+	"To the BatCave!", 
+	"To the Batmobile!", 
+    "I am ready to defeat the Joker." ,
+    "I am not prepared to defeat the Joker",
+    "The Bat-Mobile is "
 
 ];
 
-var BatmanAttacks =  ["Punch", 
-					"Kick", 
-					"BodySlam", 
-					"DropKick", 
-					"Spinning Elbow"
-
+var BatmanAttacks =  [
+	"Punch", 
+	"Kick", 
+	"BodySlam", 
+	"DropKick", 
+	"Spinning Elbow"
 ]; 
+
+var BatMobileIndicator = [
+	"Fully Operational",
+	"Initializing Self-Destruct",
+	"Deploying Bat-Pod"
+];
 
 var UtilityBelt = 1, 
 	Batarang = 1, 
@@ -78,16 +86,30 @@ return mpg;
 };  
 
 var ReportMpg = function(mpg) {
-console.log("The Batmobile gets " 
-+ mpg + " miles per gallon.");
+    console.log("The Batmobile gets " + 
+    mpg + " miles per gallon.");
 };
 
 var mpg = BatMpg(500,10);
 ReportMpg(mpg)
 
 
-
-
+for (BatMobileCondition = 100; 
+	 BatMobileCondition >10; 
+	 BatMobileCondition -- );
+	 {
+	 console.log( 
+	 		 BatmanSays[5] + " at " + 
+	 		 BatMobileCondition +
+	   	     " percent " + " and " + 
+	   	     BatMobileIndicator[0])
+	 };
+	 {
+	 console.log (
+	 		"Leaping Lizards! Situation Critical " + 
+	 		 BatMobileIndicator[1] + " " + 
+	 		 BatMobileIndicator[2])
+	 };
 
 
 
